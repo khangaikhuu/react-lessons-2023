c782c256e943170d8bae2a990c826ba45cb683cd
Khangaikhuu <khangaikhuu@gmail.com>
2023-01-02 14:14:18 +0800

    add updated jsx

--- Day-33/my-react-app-module/src/App.js
+++ Day-33/my-react-app-module/src/App.js
@@ -5,19 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Product List</h1>
+          <hr />
+          <img src="images/image.png"/>
+        </div>
       </header>
+
     </div>
   );
 }
