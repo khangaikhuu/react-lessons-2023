aaf7d3313ce6d5413b85c00c09dc95db6e21f6ef
Khangaikhuu <khangaikhuu@gmail.com>
2023-01-11 13:29:03 +0800

    update with flappy bird

--- day-38/src/App.js
+++ day-38/src/App.js
@@ -9,14 +9,14 @@ function App() {
 
   /// 1. create react state called "productList" variable in App component
   /// 2. create also changing function for productList variable in App component
-  
+
   /// 3. initialize productList state variable with products seed in App component
   /// 4. change product components products into productList variable in App component
 
   const [productList, setProductList] = useState(products)
 
-  function handleProductUpVote(productId){
-    
+  function handleProductUpVote(productId) {
+
     console.log(products)
     const newProducts = productList.map(product => {
       if (product.id === productId) {
@@ -34,28 +34,32 @@ function App() {
 
   }
 
-  const productComponents = productList.map((product) => (
-    <Product
-      key={"product-" + product.id}
-      id={product.id}
-      title={product.title}
-      description={product.description}
-      url={product.url}
-      votes={product.votes}
-      submitterAvatarUrl={product.submitterAvatarUrl}
-      productImageUrl={product.productImageUrl}
-      onVote={handleProductUpVote}
-      stars={product.stars}
-    />
-  ));
+  const productComponents = productList.map((product) => {
+    console.log(product)
+    
+    return (
+      <Product
+        key={"product-" + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={handleProductUpVote}
+        stars={product.stars}
+      />
+    )
+  });
 
   return (
-      <div className="main ui text container">
-        <h1 className="ui dividing centered header">Popular Products</h1>
-        <div id="content"></div>
-        <div className="ui unstackable items">{productComponents}</div>;
-        <Button />
-      </div>
+    <div className="main ui text container">
+      <h1 className="ui dividing centered header">Popular Products</h1>
+      <div id="content"></div>
+      <div className="ui unstackable items">{productComponents}</div>;
+      <Button />
+    </div>
   );
 }
 
--- day-40/src/App.js
+++ day-40/src/App.js
@@ -1,14 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
-import User from './components/User'
 
-function App() {
+async function App() {
+
+  // // Promise
+  // const myPromise = new Promise((resolve, rejected) => {
+  //   let isPlaying = true;
+  //   if (isPlaying) {
+  //     return resolve(isPlaying)
+  //   } else {
+  //     return rejected(isPlaying)
+  //   }
+  // })
+  // // console.log(myPromise)
+
+  // const id = 45;
+  // const URL = `https://api.jikan.moe/v4/anime/${id}`
+
+  // // const fetchResult = fetch(URL);
+  // // fetchResult
+  // //   .then(result =>
+  // //     result.json()
+  // //   ).then(data => {
+  // //     console.log(data)
+  // //   })
+
+  // const fetchedResult = await fetch(URL);
+  // console.log(fetchedResult)
+  // const JSONResult = await fetchedResult.json()
+  // console.log(JSONResult)
+ 
+
+
+
+
+
+
+  // callback function
+
+  //  function khuslen (callbackFunction){
+  //   let a = 4;
+  //   callbackFunction(a)
+  // }
+
+  // function forex(param){
+  //   console.log(param + 4)
+  // }
+
+  // khuslen(forex)
+
+  
+
+
+
+
   return (
     <div className="App">
-      <h1>Day-40 - React States</h1> 
-      <User />
+      <h1>Day-40 - React States</h1>
+
+      {/* <User /> */}
     </div>
-    
+
   );
 }
 
