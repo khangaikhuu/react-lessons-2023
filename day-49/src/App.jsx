b820e26a5e97236be1480f65ef18c883a73170a9
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2023-01-24 14:50:33 +0800

    update day 49 with timer interval with list

--- day-49/src/App.jsx
+++ day-49/src/App.jsx
@@ -1,11 +1,12 @@
 import "./style.css";
 import TimerDashboard from "./timercomponent/TimerDashboard";
+import TimersDashboard from "./components/TimersDashboard";
 
 function App() {
   return (
     <div>
       <h1>Timer App</h1>
-      <TimerDashboard />
+      <TimersDashboard />
     </div>
   );
 }
