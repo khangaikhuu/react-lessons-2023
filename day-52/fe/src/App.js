bdd583c5c62ca10749693ae6b336bb801f187fa7
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2023-02-02 22:06:46 +0800

    Update with await the functions

--- day-52/fe/src/App.js
+++ day-52/fe/src/App.js
@@ -1,97 +1,86 @@
 import './App.css';
-import { useState, useEffect } from 'react'
-import { fetchAllData, deleteUser, updateUser, createUser } from './services/usersServices';
+import {useEffect, useState} from 'react'
+import {createUser, deleteUser, fetchAllData, updateUser} from './services/usersServices';
 
 function App() {
 
-  const URL = 'http://localhost:8080/users'
-  const newUser = {
-    id: "",
-    username: "",
-    age: ""
-  }
-
-  const [users, setUsers] = useState([])
-  const [isUpdate, setIsUpdate] = useState(false)
-  const [currentUser, setCurrentUser] = useState(newUser)
-
-  useEffect(() => {
-    fetchAllData(URL, setUsers)
-  }, [])
-
-
-  async function handleDelete(userId) {
-    deleteUser(userId, URL, setUsers)
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!isUpdate) {
-      updateUser(e, URL, setUsers)
-    } else {
-      createUser(currentUser, URL, setUsers, setIsUpdate, setCurrentUser, newUser)
+    const URL = 'http://localhost:8080/users'
+    const newUser = {
+        id: "", username: "", age: ""
     }
-  }
 
-  async function handleEdit(userId) {
-    setIsUpdate(true)
-    const filteredUser = users.filter(user => user.id === userId)[0]
+    const [users, setUsers] = useState([])
+    const [isUpdate, setIsUpdate] = useState(false)
+    const [currentUser, setCurrentUser] = useState(newUser)
 
-    if (filteredUser) {
-      setCurrentUser({
-        id: filteredUser.id,
-        age: filteredUser.age,
-        username: filteredUser.username
-      })
+    useEffect(() => {
+        fetchAllData(URL, setUsers).then(r => console.log(r))
+    }, [])
+
+
+    async function handleDelete(userId) {
+        await deleteUser(userId, URL, setUsers)
     }
-  }
 
-  function handleUserName(e) {
-    setCurrentUser({
-      ...currentUser,
-      username: e.target.value
-    })
-  }
+    async function handleSubmit(e) {
+        e.preventDefault();
 
-  function handleUserAge(e) {
-    setCurrentUser({
-      ...currentUser,
-      age: e.target.value
-    })
-  }
+        if (!isUpdate) {
+            await updateUser(e, URL, setUsers)
+        } else {
+            await createUser(currentUser, URL, setUsers, setIsUpdate, setCurrentUser, newUser)
+        }
+    }
 
-  return (
-    <div className="App">
-      <h1>Day-52 - NodeJS FS Module</h1>
-      <h3>Create User FORM</h3>
-      <form onSubmit={handleSubmit}>
-        <label>
-          User Name:
-          <input name="username" value={currentUser.username} onChange={handleUserName} />
-        </label>
-        <br />
-        <label>
-          Age:
-          <input name="age" value={currentUser.age} onChange={handleUserAge} />
-        </label>
-        <br />
-        <button>{isUpdate ? 'Update' : 'Submit'}</button>
-      </form>
-      <h3>Users List</h3>
-      {
-        users && users.map((user, index) => {
-          return (
-            <div key={index}>
-              <p>{user.username} : {user.age} {" "} <button onClick={() => handleDelete(user.id)}>Delete</button>
-                <button onClick={() => handleEdit(user.id)}>Edit</button>
-              </p>
-            </div>
-          )
+    async function handleEdit(userId) {
+        setIsUpdate(true)
+        const filteredUser = users.filter(user => user.id === userId)[0]
+
+        if (filteredUser) {
+            setCurrentUser({
+                id: filteredUser.id, age: filteredUser.age, username: filteredUser.username
+            })
+        }
+    }
+
+    function handleUserName(e) {
+        setCurrentUser({
+            ...currentUser, username: e.target.value
         })
-      }
-    </div>
-  );
+    }
+
+    function handleUserAge(e) {
+        setCurrentUser({
+            ...currentUser, age: e.target.value
+        })
+    }
+
+    return (<div className="App">
+        <h1>Day-52 - NodeJS FS Module</h1>
+        <h3>Create User FORM</h3>
+        <form onSubmit={handleSubmit}>
+            <label>
+                User Name:
+                <input name="username" value={currentUser.username} onChange={handleUserName}/>
+            </label>
+            <br/>
+            <label>
+                Age:
+                <input name="age" value={currentUser.age} onChange={handleUserAge}/>
+            </label>
+            <br/>
+            <button>{isUpdate ? 'Update' : 'Submit'}</button>
+        </form>
+        <h3>Users List</h3>
+        {users && users.map((user, index) => {
+            return (<div key={index}>
+                <p>{user.username} : {user.age} {" "}
+                    <button onClick={() => handleDelete(user.id)}>Delete</button>
+                    <button onClick={() => handleEdit(user.id)}>Edit</button>
+                </p>
+            </div>)
+        })}
+    </div>);
 }
 
 export default App;
