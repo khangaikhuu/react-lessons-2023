bdedac84bf07260837717ac1ab313b2759b6734c
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2023-03-15 23:26:32 +0800

    add: remove and delete functions to the api

--- day-77-mongoose-setup/backend/routes/api.js
+++ day-77-mongoose-setup/backend/routes/api.js
@@ -39,11 +39,53 @@ Router.get("/users", async (request, response) => {
   response.json({ data: result });
 });
 
-Router.delete("/user", async (request, response) => {
-  const body = request.body;
-  console.log(body);
+Router.put("/updateOneUser", async (request, response) => {
+  const result = await User.updateOne(
+    { email: "simon-sinek@gmail.com" },
+    { $set: { lastLogin: Date.now() } }
+  );
 
-  const result = await User.findOneAndDelete({ _id: body.id });
+  response.json({ data: result });
+});
+
+Router.put("/findAndUpdateOneUser", async (request, response) => {
+  const result = await User.findOneAndUpdate(
+    { email: "simon-sinek@gmail.com" },
+    { $set: { lastLogin: Date.now() } }
+  );
+
+  response.json({ data: result });
+});
+
+Router.put("/findByIdAndUpdateUser", async (request, response) => {
+  const result = await User.findByIdAndUpdate(
+    { _id: "64118aa3f8bdafdb3a15a2d1" },
+    { $set: { lastLogin: Date.now() } }
+  );
+
+  response.json({ data: result });
+});
+
+Router.delete("/findOneAndDeleteUser", async (request, response) => {
+  const body = request.body;
+
+  const result = await User.findOneAndDelete({ email: body.email });
+
+  response.json({ data: result });
+});
+
+Router.delete("/deleteOneUser", async (request, response) => {
+  const body = request.body;
+
+  const result = await User.deleteOne({ _id: body.id });
+
+  response.json({ data: result });
+});
+
+Router.delete("/findByIdAndDeleteUser", async (request, response) => {
+  const body = request.body;
+
+  const result = await User.findByIdAndDelete({ _id: body.id });
 
   response.json({ data: result });
 });
