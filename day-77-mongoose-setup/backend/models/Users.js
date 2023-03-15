9c8179bb7703be8768a72e23deaf32ff47f779ca
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2023-03-15 18:20:49 +0800

    add: update user by email

--- day-77-mongoose-setup/backend/models/Users.js
+++ day-77-mongoose-setup/backend/models/Users.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String,
+  email: { type: String, unique: true },
   createdOn: Date,
+  modifiedOn: { type: Date, default: Date.now },
+  lastLogin: Date,
 });
+
+userSchema.statics.findByUserEmail = function (userEmail) {
+  return this.find({ email: userEmail }, "_id name email", {
+    sort: "modifiedOn",
+  });
+};
 
 const User = mongoose.model("user", userSchema);
 
--- day-77-mongoose-setup/backend/routes/api.js
+++ day-77-mongoose-setup/backend/routes/api.js
@@ -2,23 +2,41 @@ const express = require("express");
 const User = require("../models/Users");
 const Router = express.Router();
 
-Router.get("/users", async (request, response) => {
-  const result = await User.find({});
-  console.log(result);
-  response.json({ data: result });
-});
-
 Router.post("/user", async (request, response) => {
   const body = request.body;
 
   const newUser = new User(body);
-
   const result = await newUser.save();
-  console.log(result);
 
   response.json({
     data: result,
   });
+});
+
+Router.get("/userByName", async (request, response) => {
+  const result = await User.findOne(
+    { name: "Simon Sinek" }, // users called Simon Sinek
+    null, // returning all fields in model
+    { sort: { lastLogin: -1 } } // sorted by lastLogin descending
+  );
+
+  response.json({
+    data: result,
+  });
+});
+
+Router.get("/userByEmail", async (request, response) => {
+  const result = await User.findByUserEmail("simon-sinek@gmail.com");
+
+  response.json({
+    data: result,
+  });
+});
+
+Router.get("/users", async (request, response) => {
+  const result = await User.find({});
+  console.log(result);
+  response.json({ data: result });
 });
 
 Router.delete("/user", async (request, response) => {
