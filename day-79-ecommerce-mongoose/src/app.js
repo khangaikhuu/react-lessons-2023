81d6b4d1757530adda59730740439b3d95b15fa3
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-03-16 16:02:59 +0800

    update: nodejs

---
+++ day-79-ecommerce-mongoose/src/app.js
@@ -0,0 +1,13 @@
+import express, { json } from 'express';
+
+const app = express();
+
+app.use(json())
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+    res.json({ status: true, message: "Our node.js app works" })
+});
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
