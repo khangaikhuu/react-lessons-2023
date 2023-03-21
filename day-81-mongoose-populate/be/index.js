a4585f7be7fc9364eefce1713c6d3a56491c629e
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-03-21 11:41:16 +0800

    add day 81 with insert many, delete and update many

--- day-81-mongoose-populate/be/index.js
+++ day-81-mongoose-populate/be/index.js
@@ -18,9 +18,40 @@ app.get("/", (request, response) => {
   response.send("<h1>Day - 81: Insert/Update/Delete Many</h1>");
 });
 
+app.get("/list", async (request, response) => {
+  const result = await Todo.find({});
+
+  response.status(200).json({
+    data: result,
+  });
+});
+
 app.post("/create", async (request, response) => {
   const body = request.body;
   const result = await Todo.insertMany(body);
+
+  response.status(200).json({
+    data: result,
+  });
+});
+
+app.put("/update", async (request, response) => {
+  const body = request.body;
+
+  const result = await Todo.updateMany(
+    { name: body[1].name },
+    { $set: { checked: body[0].checked } }
+  );
+
+  response.status(200).json({
+    data: result,
+  });
+});
+
+app.delete("/delete", async (request, response) => {
+  const body = request.body;
+
+  const result = await Todo.deleteMany(body);
 
   response.status(200).json({
     data: result,
