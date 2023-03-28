d3c0cc5afcb39d4ea9389c8406813c8a494c8edf
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-03-28 09:30:25 +0800

    update

--- day-86-mongodb-transaction/be/index.js
+++ day-86-mongodb-transaction/be/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const categoryApi = require('./routes/category.routes');
-const productApi = require('./routes/product.routes');
-const transactionApi = require('./routes/transaction-routes');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const categoryApi = require("./routes/category.routes");
+const productApi = require("./routes/product.routes");
+const transactionApi = require("./routes/transaction-routes");
 
 const app = express();
 const port = process.env.PORT;
@@ -13,14 +13,18 @@ const MONGO_DB_URL = process.env.MONGO_DB_URL;
 app.use(express.json());
 app.use(cors());
 
-app.use('/product', productApi);
-app.use('/category', categoryApi);
-app.use('/transaction', transactionApi);
+app.get("/", (req, res) => {
+  res.send("<h1>Day 86 - MongoDB Transaction</h1>");
+});
+
+app.use("/product", productApi);
+app.use("/category", categoryApi);
+app.use("/transaction", transactionApi);
 
 app.listen(port, () => {
-    mongoose
-        .connect(MONGO_DB_URL)
-        .then(() => console.log("Database connected successfully"))
-        .catch((error) => console.error(error));
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  mongoose
+    .connect(MONGO_DB_URL)
+    .then(() => console.log("Database connected successfully"))
+    .catch((error) => console.error(error));
+  console.log(`Example app listening at http://localhost:${port}`);
+});
