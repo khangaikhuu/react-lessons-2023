ef55693fc994d6c9969964e221ec36428ada132a
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-03-28 11:27:04 +0800

    update with transaction

--- day-86-mongodb-transaction/be/controller/transaction-controller.js
+++ day-86-mongodb-transaction/be/controller/transaction-controller.js
@@ -1,10 +1,22 @@
 const User = require("../models/User");
 const ShippingAddress = require("../models/ShippingAddress");
+const Product = require("../models/Product");
 
 exports.createTransaction = async (req, res) => {
   try {
     const session = await User.startSession();
     session.startTransaction();
+    const product = await Product.updateOne(
+      {
+        _id: "64225a2a79943abb5ecc44fc",
+      },
+      {
+        $inc: { quantity: -2 },
+      },
+      { session }
+    );
+
+    console.log("product", product);
     const user = await User.create(req.body.user, { session });
     const shippingAddress = await ShippingAddress.create(
       req.body.shippingAddress,
@@ -12,7 +24,7 @@ exports.createTransaction = async (req, res) => {
     );
     await session.commitTransaction();
     session.endSession();
-    res.json({ status: true, user, shippingAddress });
+    res.json({ status: true, user, shippingAddress, product });
   } catch (error) {
     res.json({ status: false, error });
   }
@@ -37,12 +49,24 @@ exports.createTransactionWithRollback = async (req, res) => {
 
 exports.createTransactionWithOutSession = async (req, res) => {
   try {
+    const product = await Product.updateOne(
+      {
+        _id: "64225a2a79943abb5ecc44fc",
+      },
+      {
+        $inc: { quantity: -2 },
+      }
+    );
+
+    console.log("product", product);
+
     const user = await User.create(req.body.user);
+
     const shippingAddress = await ShippingAddress.create({
       address: req.body.shippingAddress,
       user_id: user._id,
     });
-    res.json({ status: true, user, shippingAddress });
+    res.json({ status: true, user, shippingAddress, product });
   } catch (error) {
     res.json({ status: false, error });
   }
