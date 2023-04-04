409a5c69a23bdb30f5dd4439d5855c99a2bad283
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-04-04 12:23:59 +0800

    update: with pagination

--- day-90-express-typescript/be/controllers/theater.controller.ts
+++ day-90-express-typescript/be/controllers/theater.controller.ts
@@ -2,8 +2,13 @@ import TheaterModel from "../models/theaters.model";
 import { Request, Response } from "express";
 
 export const getTheaters = async (req: Request, res: Response) => {
+  const page: number = Number(req.query.page) || 0;
+  const moviesPerPage: number = Number(req.query.moviesPerPage) || 5;
+
   try {
-    const theaters = await TheaterModel.find({});
+    const theaters = await TheaterModel.find({})
+      .limit(moviesPerPage)
+      .skip(moviesPerPage * page);
     res.status(200).json(theaters);
   } catch (error) {
     res.status(404).json({ data: [] });
