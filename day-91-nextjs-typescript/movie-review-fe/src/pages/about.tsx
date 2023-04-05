6b620f14888a20a8a04ec39c72b0a1d3c8156948
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-04-05 16:16:50 +0800

    day: 91 update with about image

--- day-91-nextjs-typescript/movie-review-fe/src/pages/about.tsx
+++ day-91-nextjs-typescript/movie-review-fe/src/pages/about.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 export async function getStaticProps() {
@@ -16,6 +17,16 @@ export default function About(props: any): JSX.Element {
       <h1>Its an About page</h1>
       <div>
         <p>Theater Name: {props.content}</p>
+      </div>
+      <div>
+        <div style={{ width: 500, height: 200, position: "relative" }}>
+          <Image
+            src="https://images.unsplash.com/photo-1605460375648-278bcbd579a6"
+            fill
+            style={{ objectFit: "cover" }}
+            alt="Picture of the author"
+          />
+        </div>
       </div>
     </div>
   );
