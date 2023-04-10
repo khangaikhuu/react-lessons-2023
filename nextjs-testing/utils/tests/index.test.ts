9cda1dbe17bf903ac48bd5a859d6b5cf92b37cdf
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-04-10 17:29:12 +0800

    add day 96 for testing

---
+++ nextjs-testing/utils/tests/index.test.ts
@@ -0,0 +1,13 @@
+import { cutTextToLength } from "../index";
+
+const str = "The quick brown fox jumps over the lazy dog.";
+
+describe("cutTextToLength", () => {
+  it("should cut text to length", () => {
+    expect(cutTextToLength(str, 20)).toBe("The quick brown fox ...");
+  });
+
+  it("should not cut text to length", () => {
+    expect(cutTextToLength(str, 100)).toBe(str);
+  });
+});
