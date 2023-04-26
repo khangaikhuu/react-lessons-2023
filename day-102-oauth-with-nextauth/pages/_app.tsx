a53511f957033ba69fd56030b381974d536c5023
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2023-04-26 10:24:56 +0800

    add: github google and other providers for oauth

---
+++ day-102-oauth-with-nextauth/pages/_app.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
